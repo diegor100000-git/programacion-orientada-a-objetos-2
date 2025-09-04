1c51d7d17dee6a4840e1c6572cf686b62472b9b4
DanielaLis14 <daniela.lisbet14@gmail.com>
2025-09-04 11:39:31 -0500

    sesion 4 J

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
@@ -3,7 +3,7 @@ package pe.edu.upeu.asistencia;
 public class Aplication {
 
     public static void main(String[] args) {
-        System.out.println("iniciando asistencia drza");
+        System.out.println("iniciando asistencia");
         AsistenciaApplication.main(args);
     }
 }
